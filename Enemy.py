3980705bd221c400fd386a29a910bdac69f33ae1
LucasPereiraWork <lucaspereirainfowork@gmail.com>
2025-07-21 22:32:21 +0100

    Enemy

---
+++ Enemy.py
@@ -0,0 +1,33 @@
+# This module defines the enemy behaviour
+
+import pygame
+import enemy_cfg
+import game_cfg
+
+class Enemy:
+    def __init__(self, pos):
+        self.pos = pygame.Vector2(pos)
+        self.image = pygame.image.load(enemy_cfg.enemy_sprite).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (enemy_cfg.width, enemy_cfg.height))
+        self.rect = self.image.get_rect(center=self.pos)
+
+
+    def handle_events(self, delta_time):
+        pass
+
+
+    def update(self, delta_time):
+        pass
+
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def is_off_screen(self, screen_width, screen_height):
+        if (self.pos.x < 0 or self.pos.x > screen_width or
+            self.pos.y < 0 or self.pos.y > screen_height):
+            game_cfg.objects_to_remove.append(self)
+    
+    def defeated(self):
+        game_cfg.objects_to_remove.append(self)
+        #particles = game_cfg.create_particles(self.pos, enemy_cfg.particle_count)#game_cfg.objects_to_instace.extend(particles)
---
+++ enemy_cfg.py
@@ -0,0 +1,9 @@
+# This module defines the enemy behaviour
+
+enemy_width = 30
+enemy_height = 30
+enemy_speed = 100
+default_enemy_rotation = 0
+enemy_detection_radius = 200
+enemy_detection_angle = 90
+enemy_sprite = "Resources/Sprites/Enemy/EnemyPrototype.png"
